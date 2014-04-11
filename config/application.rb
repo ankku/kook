f9b9525199657b6b84bfc76e1279cee3a80ec319
ankku <ankku.lipiainen@gmail.com>
2014-04-11 13:19:57 +0300

    precompile

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.precompile += [
-    'application.css.erb'
-]
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -24,5 +19,11 @@ module Kook
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+
+    config.assets.precompile += [
+        'application.css.erb'
+    ]
+    
   end
 end
